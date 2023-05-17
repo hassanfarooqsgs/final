af1c6ac35b7de840d7378525a339caf347048c9d
hassan_farooq <hassanfarooqsgs@gmail.com>
2023-05-17 17:57:57 +0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin = User.new(
+    email: 'hassan@example.com',
+    password: 'admin_password',
+    password_confirmation: 'admin_password',
+    admin: true
+  )
+  admin.save
+  
