f13d1cfb9b2c02639c5a82f74f84c3648a6a3b2e
hassan_farooq <hassanfarooqsgs@gmail.com>
2023-05-18 19:28:36 +0500

    view edit delete  done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,38 @@ class UsersController < ApplicationController
     def index
       @users = User.all
     end
+  
+  
+  def show
+    @user = User.find(params[:id])
   end
-  
-  
+
+  def edit
+  @user = User.find_by_id(params[:id])
+  if @user.nil?
+    # Handle the case when the user record is not found
+    redirect_to root_path, alert: 'User not found.'
+  end
+end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "User successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+  params.require(:user).permit(:email, :country, :first_name, :last_name, :middle_name, :phone_number)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "User successfully deleted."
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index]
+  resources :users
+  #resources :users, except: [:edit, :update, :destroy]
 
+ 
 root "users#index"
 end
