2f4a687e843edf9f66c4431fea1eff960c2e96e7
hassan_farooq <hassanfarooqsgs@gmail.com>
2023-05-22 16:27:19 +0500

    refactored code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,61 +1,62 @@
 class UsersController < ApplicationController
-    def index
-
-      @users = if params[:search]
-      User.where("first_name LIKE :search OR last_name LIKE :search OR email LIKE :search OR id = :id",
-                 search: "%#{params[:search]}%", id: params[:search].to_i)
-     
-     else
-      User.order(params[:sort]).page(params[:page]).per(3)
-
-     end
-    end
+  before_action :set_user, except: %i[index] 
   
-  
-    def show
-    @user = User.find(params[:id])
+  def index
+    if params[:search]
+      @users = User.search(params[:search])
+    else
+      @users = User
+        .order(params[:sort])
+        .page(params[:page])
+        .per(3)
     end
+  end
 
-    
-  
-    def edit
-    @user = User.find_by_id(params[:id])
-    if @user.nil?
-    # Handle the when the user record is not found
-    redirect_to root_path, alert: 'User not found.'
-    end
 
-    end
-    
-    def update
-    @user = User.find(params[:id])
+  def show; end
+
+  def edit
+    redirect_to root_path, alert: 'User not found.' if user.nil?
+  end
+  
+  def update
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "User successfully updated."
     else
       render :edit
     end
-    end
+  end
 
 
-    def export_csv
-    @users = User.all
-      require 'csv'
-      respond_to do |format|
-        format.csv { send_data @users.to_csv, filename: 'users.csv' }
-      end
+  def export_csv
+  @users = User.all
+    require 'csv'
+    respond_to do |format|
+      format.csv { send_data @users.to_csv, filename: 'users.csv' }
     end
-    
-    def destroy
-    @user = User.find(params[:id])
+  end
+  
+  def destroy
     @user.destroy
+    
     redirect_to users_path, notice: "User successfully deleted."
-    end
+  end
 
-     private
-    def user_params
-      params.require(:user).permit(:email, :country, :first_name, :last_name, :middle_name, :phone_number, :image)
+  private
 
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-   
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :country,
+      :first_name,
+      :last_name,
+      :middle_name,
+      :phone_number,
+      :image
+    )
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,36 @@
 class User < ApplicationRecord
-       validates :first_name, :middle_name, :last_name, :country, :phone_number, presence: true
+  validates :first_name, :middle_name, :last_name, presence: true
+  validates :country, :phone_number, presence: true
        
-    has_one_attached :image
+  has_one_attached :image
 
   
   def self.to_csv
     CSV.generate(headers: true) do |csv|
-      csv << ['FirstName','MiddleName','LastName','Email', 'Country']  # Add column headers
+      csv << ['FirstName','MiddleName','LastName','Email', 'Country']
 
       all.each do |user|  # Iterate over the users
-        csv << [user.first_name, user.middle_name,user.last_name,user.email, user.country]  # Add user data to CSV
+        csv << [
+          user.first_name,
+          user.middle_name,
+          user.last_name,
+          user.email,
+          user.country
+        ]
       end
     end
   end
+
+  def self.search(attribute)
+    where(
+      'first_name LIKE :search OR last_name LIKE :search OR email LIKE :search OR id = :id',
+      search: "%#{attribute}%",
+      id: attribute.to_i
+    )
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
-
